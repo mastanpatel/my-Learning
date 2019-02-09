51664011b60144f1c352ae9dcca87a03f2e044e4
Mastan Patel <mastanpatel1993@gmail.com>
2019-02-09 14:24:25 +0530

    completed Learning till methods

--- C sharp Learnings/c sharp Fundamentals/Program.cs
+++ C sharp Learnings/c sharp Fundamentals/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//namespace diclaration
+using System;
 
 namespace c_sharp_Fundamentals
 {
@@ -6,8 +7,138 @@ namespace c_sharp_Fundamentals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow world");
-            Console.WriteLine("Hellow world");
+            // Reading From Console
+            string UserName = Console.ReadLine();
+
+            //concatination method to print to console
+            Console.WriteLine("Hello "+ UserName);
+
+            //Place holder Syntax --Preferred
+            Console.WriteLine("Hello {0}", UserName);
+
+            //Built in Types in c#
+
+            //bool true/fasle
+
+            //---integral data types----
+
+            //sbyte (-128 to 127) it is signed byte -- 8 bits integer
+            //byte (0 to 255)  -- 8 bits integer
+            //char (unicode) it is -- 16 bit character
+            //short (signed) -- 16 bit integer
+            //ushort(unsigned) -- 16 bit integer
+            //int (signed) -- 32 bit integer (-2,xxx,xxx,xxx to 2,xxx,xxx,xxx)
+            //uint (unsigned) -- 32 bit integer (0 to 4,xxxx,xxx,xxx)
+            //long (signed) -- 64 bit integer (-9,xxx,xxx,xxx,xxx,xxx,xxx to 9,xxx,xxx,xxx,xxx,xxx,xxx)
+            //ulong (unsigned) --64 bit intger (0 to 18,xxx,xxx,xxx,xxx,xxx,xxx)
+
+            //-- Floating data types
+            //float --32 bit (7 digits after decimal point)-- precision
+            //double --64 bit (15 to 16 digits after decimal points) --precision
+
+            //Decimal --128 bit (28-29 precision)
+
+            // string Types
+            //verbatim literal --@  --used mosthly in file names folder names
+            //Console.WriteLine(@"a/b/c/d.txt");   --used to remove escape sequence
+            //ways to find the int range from code
+            Console.WriteLine("max {0}, min {1}",int.MaxValue, int.MinValue);
+            Console.WriteLine("max {0}, min {1}", Byte.MaxValue, Byte.MinValue);
+            Console.WriteLine("max {0}, min {1}", long.MaxValue, long.MinValue);
+
+
+            //Common Operators in c#
+            //ternary operator
+            //----
+            //bool isNumber10 = number == 10 ? true : false;
+            //----
+
+            // Types in c#
+            // 1. value type -- int,float,double,struct,enum etc...  --- intialised as 0
+            // 2. reference type -- class, interface, delegates, arrays,string etc.. --- initialised as null
+
+            //------
+            //nullable types
+            //ex:-  int i = null; --gives error because int cannot be null in intialise state
+            //       int? i = null; -- works fine (because now it is nullable type)
+            //mostly this scenario occures when we need to store null in database but get the we cannot get null in code
+            //ex :- if in user form there is feild to fill are u above 18?
+            // the user will enter 'y' or 'n' or he wont enter any thing then the null value is stored in database
+            //to handle such scenario c# introduced nullable data types
+            //------- 
+
+
+
+            //--- Null Coalesing Operator ?? ----
+            // int? ticketAvailable = null;
+            //int availableTickets = ticketAvailable ?? 0;  --using null coalesing operator to convert nullble to non nullable
+            //---------
+
+
+
+            //--- type conversion ---
+            // 1. implicite conversion
+            // this happens automatically id there is no data loss between data type
+            //ex : int to flaot conversion
+
+            // 2. explicie conversion
+            // this convertion is done using cast () --operator   ex:- int i = (int) b;  b--> is flaot value
+            // another method is using Convert.To method  ex: int i = Convert.Toint32(b);
+
+            //imp-- if the data type value is stored as string then use "Parse" or "TryParse" methods
+            // ex : string number = "1234";
+            //      int numberValue = int.Parse(number);
+            //use Try parse if ur not sure the given string is convertable to int or not
+            // ex : string number = "12DE";
+            //      int numberValue = int.Parse(number);   -- exception is trown so to avoid it use TryParse
+            //      int.TryParse(number, out numberValue);  -- it will return bool value
+            //-----------------------
+
+
+            //-----------Arrays in c# -------
+            // array are stongly types
+            // int[] array = new int[3];
+            //-------------------------------
+
+            //------- Comments in c#--------
+            //single line comment -- //
+            // multiline comment -- /* */
+            // very imp --> XML Documentation Comment -- ///
+            // XML comments are used to show the documentation comments of any funtion, class etc
+            //ex:- 
+            //  //<summary> this funtion add two numbers
+                //</summary>
+            // public void add(int a,int b){};
+            
+            //-------------------------------
+
+
+
+            //------ Diference between & and &&,  and | and || --------
+            // very Imp ---
+            // if(number>0 && number<5)   -- here if number>0 is false then it won't check second condition number<5
+            // if(number>0 & number<5)    -- here if number>0 is false then also it will check second consition number<5
+            // .. similarly for | and || operators
+            // so for performance benefit we use && instead of &   and || instead of |
+            //----------------------------------------------------------
+
+
+
+            //------ goto statement ------
+            // start:     -- this is lable where the code shown jump and go
+            // goto start;  -- this way we can use to goto perticular Line of Program -- here it will go to(jump to) start: line code
+            // it bad practice to use goto statement because it become very difficult (complex) to "debug" the code as program start jumping between lines of code.
+            // this also can be achived using loops ex: do-while loop -- prefered way of writing code.
+            //----------------------------
+
+
+            //--- methods in c#------------
+            //-----------------------------
+            // refer methodLearning.cs class
+            //-----------------------------
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ C sharp Learnings/c sharp Fundamentals/methodsLearning.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace c_sharp_Fundamentals
+{
+    /// <summary>
+    /// this Class is Used for Learning about Methods in c#
+    /// </summary>
+    class methodsLearning
+    {
+        /// <summary>
+        /// this is an static method
+        /// </summary>
+        /// [accessModifiers][modifier][returnType][methodName]()   -->this method signature
+        public static void MainMethod()
+        {
+            //we cannot call directly other non static methods in this method
+            //ex : printInstanceMethod();  --> this method will show swilly error line as we can't call non static members in static method.
+
+            // we can call other static method in static method as below printStaticMethod(); is called successfully.
+            printStaticMethod();
+
+            //--- calling non static methods ---//
+            //to call non static method we need to create instace of class
+            methodsLearning methods = new methodsLearning();
+
+            //now using instance we can call non static methods
+            methods.printInstanceMethod();
+
+        }
+
+        /// <summary>
+        /// this is an instace method we are called by creating instance of the class.
+        /// </summary>
+        public void printInstanceMethod()
+        {
+            Console.WriteLine("My name is instance method");
+        }
+
+        /// <summary>
+        /// this is an static method called directly without creating instance of class.
+        /// </summary>
+        public static void printStaticMethod()
+        {
+            Console.WriteLine("my name is Static method");
+        }
+
+
+        // Method Parameter Types
+        // 1. Value Parameters
+        // 2. Reference Parameters
+        // 3. Out Parameters
+        // 4. Parameter Arrays
+
+        public void TestMethod()
+        {
+            //calling value parameter method
+            int a = ValaueParameterMethod(1);
+
+
+            //calling reference parameter method
+            //here the actual value of "a" get updated as we are passing adress of a as parameter.
+            int b = ReferenceParameterMethod(ref a);
+
+            int sum = 0;
+            int product = 0;
+            // calling output parameter type method to return multiple values from single method
+            OutputParameterMethod(a, b,out sum,out product);
+
+
+            // calling as parameter array to pass as array
+            ParamParameterMethod(1, 2, 3, 4, 5);
+
+        }
+
+        /// <summary>
+        /// this is an value Parameter method where value is passed as parameter to this method
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns>return the intiger</returns>
+        public int ValaueParameterMethod(int a)
+        {
+            return a;
+        }
+
+
+        /// <summary>
+        /// this is an reference parameter method where the parameter is passed as an refrence key word "ref" is essential
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns>return the updated value of a</returns>
+        public int ReferenceParameterMethod(ref int a)
+        {
+            return a + 2;
+        }
+
+
+
+        /// <summary>
+        /// this method is of output parameter type where we pass output parameter to return multiple values in from single method
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="sum"></param>
+        /// <param name="product"></param>
+        public void OutputParameterMethod(int a,int b,out int sum,out int product)
+        {
+            sum = a + b;
+            product = a * b;
+        }
+
+
+        public void ParamParameterMethod(params int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
