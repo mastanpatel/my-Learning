723c896cb47cf1081325530c856dbe34c37c64a1
Mastan Patel <mastanpatel1993@gmail.com>
2019-02-09 07:04:42 +0530

    hello

---
+++ C sharp Learnings/c sharp Fundamentals/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c_sharp_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow world");
+            Console.WriteLine("Hellow world");
+        }
+    }
+}
