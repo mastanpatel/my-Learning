b5b0be978620902cae6c754011607d25b4110524
mastanpatel <mastanpatel1993@gmail.com>
2019-05-05 06:01:52 +0530

    done error correction

--- Data Structure and Algorithms/Data Structure and Algorithms/Program.cs
+++ Data Structure and Algorithms/Data Structure and Algorithms/Program.cs
@@ -6,85 +6,86 @@ namespace Data_Structure_and_Algorithms
     {
         static void Main(string[] args)
         {
-            /*   Data Strucutres:
-             It is an Method of Storing and Organising a Data so that it can be used efficieantly.
+            Console.Write("Hello");
+            //    Data Strucutres:
+            //  It is an Method of Storing and Organising a Data so that it can be used efficieantly.
 
 
-            we Talk about Data Strucures in two ways:
+            // we Talk about Data Strucures in two ways:
 
-            1.Abstract type also known as (ADT) abstract data type
-              where we have the operation and features of Data structure
-            2. Implemetation of Data structure is done using any Language like c#, python etc.. 
-             (implemetating how to organise and operation of perticular data strucutre)
-
-
-            What is an Abstract Data Type:
-            -> it define Data and Operations on that data wirhot Implemetation.
-
-            How we Study Data Strucutres
-
-            1. we Study Logical View of Data Strucutre
-            2. we Study various operations done on Data structure
-            3. we Study Cost of Operations (Time and Memory Complexity of Data Structure)
-            4. we Implement these Data Structure using C#
+            // 1.Abstract type also known as (ADT) abstract data type
+            //   where we have the operation and features of Data structure
+            // 2. Implemetation of Data structure is done using any Language like c#, python etc.. 
+            //  (implemetating how to organise and operation of perticular data strucutre)
 
 
+            // What is an Abstract Data Type:
+            // -> it define Data and Operations on that data wirhot Implemetation.
 
-            *** Very Important Lesson***//   //*** Interview wise and Competative Programing Wise***//
-                                             /* 
-                                             Take the Given Problem (Understand Problem thoroughly.
-                                             List Down Type of Data and Operations needed in this Problem and Size of the Data.
-                                             identify the most Heavily(Frequient) performed Operation (insertion, deletion, iteration etc...)
-                                             choose the Basic Data Strucutre mostly suited to implement above mevtioned Data strucuture and its Operations
-                                             with efficient cost (less costly)
-                                             choose to implement From primitive sata strucute like (Array, LinkedList, Stack, Queue, Tree, Graph, Hash etc....)
-                                             Optimise if any where Required.
+            // How we Study Data Strucutres
 
-
-                                             Important Sylaabus for DS and Algorithms
+            // 1. we Study Logical View of Data Strucutre
+            // 2. we Study various operations done on Data structure
+            // 3. we Study Cost of Operations (Time and Memory Complexity of Data Structure)
+            // 4. we Implement these Data Structure using C#
 
 
-                                       Data Structure and Algorithms
 
-                                     1. Arrays  -- 19
-                                        - Subarray with given sum -- (i have learn't to make code funtional and first solve for small input and them using brutforce or
-                                          whatever method hit u at that point
-                                          and then try to Optimise it (Time Comlexity and Space complexity) --> look at the Operations Performed on each element at worst case and try to reduce it.
+            // *** Very Important Lesson***//   //*** Interview wise and Competative Programing Wise***//
+            //                                  /* 
+            //                                  Take the Given Problem (Understand Problem thoroughly.
+            //                                  List Down Type of Data and Operations needed in this Problem and Size of the Data.
+            //                                  identify the most Heavily(Frequient) performed Operation (insertion, deletion, iteration etc...)
+            //                                  choose the Basic Data Strucutre mostly suited to implement above mevtioned Data strucuture and its Operations
+            //                                  with efficient cost (less costly)
+            //                                  choose to implement From primitive sata strucute like (Array, LinkedList, Stack, Queue, Tree, Graph, Hash etc....)
+            //                                  Optimise if any where Required.
 
-                                        - Kadane’s Algorithm -- (solved it '\/' and learnt to minimise the steps (reduce the lines of code and remove unnessary things, use inbuild funtionality of c#,
-                                                                 try to cover all corner cases.)
 
-                                        - Missing number in array -- (solved using sumFormula)
-                                                                  -- did some mistakes
-                                                                     - Not reading Q's Properly (missing dat format like sorted or not and missing corner cases)
-                                                                     - *** repeating same mistake of not rechecking code for errors and Optimisation.
-                                                                     --- don't repeat above mistakes
-                                        - Sort an array of 0s, 1s and 2s -- (solved)
+            //                                  Important Sylaabus for DS and Algorithms
 
-                                        - Equilibrium point -- (solved)   
 
-                                        -    
+            //                            Data Structure and Algorithms
 
-                                     2. string -- 15
-                                     3. Linked List -- 17
-                                     4. stack and Queue -- 8
-                                     5. Tree and BST -- 16
-                                     6. Heap -- 6
-                                     7. Recursion -- 6
-                                     8. Hashing -- 15
-                                     9. Graph -- 15
-                                     10. Greedy -- 15
-                                     11. Dynamic Programming -- 15
-                                     12. Devide and Conquir -- 6
-                                     13. BackTracking -- 5
-                                     14. Bitmagic -- 14
+            //                          1. Arrays  -- 19
+            //                             - Subarray with given sum -- (i have learn't to make code funtional and first solve for small input and them using brutforce or
+            //                               whatever method hit u at that point
+            //                               and then try to Optimise it (Time Comlexity and Space complexity) --> look at the Operations Performed on each element at worst case and try to reduce it.
 
-                                     -----------------------
-                                     Total :-- 176            avalable Time : 4 + 10 + 4 + 4 + 4 + 10 + 10 + 4 + 4 ~ 50 Hrs
+            //                             - Kadane’s Algorithm -- (solved it '\/' and learnt to minimise the steps (reduce the lines of code and remove unnessary things, use inbuild funtionality of c#,
+            //                                                      try to cover all corner cases.)
 
-                                     176/50 ~ 4 problems per hour
+            //                             - Missing number in array -- (solved using sumFormula)
+            //                                                       -- did some mistakes
+            //                                                          - Not reading Q's Properly (missing dat format like sorted or not and missing corner cases)
+            //                                                          - *** repeating same mistake of not rechecking code for errors and Optimisation.
+            //                                                          --- don't repeat above mistakes
+            //                             - Sort an array of 0s, 1s and 2s -- (solved)
 
-                                     */
+            //                             - Equilibrium point -- (solved)   
+
+            //                             -    
+
+            //                          2. string -- 15
+            //                          3. Linked List -- 17
+            //                          4. stack and Queue -- 8
+            //                          5. Tree and BST -- 16
+            //                          6. Heap -- 6
+            //                          7. Recursion -- 6
+            //                          8. Hashing -- 15
+            //                          9. Graph -- 15
+            //                          10. Greedy -- 15
+            //                          11. Dynamic Programming -- 15
+            //                          12. Devide and Conquir -- 6
+            //                          13. BackTracking -- 5
+            //                          14. Bitmagic -- 14
+
+            //                          -----------------------
+            //                          Total :-- 176            avalable Time : 4 + 10 + 4 + 4 + 4 + 10 + 10 + 4 + 4 ~ 50 Hrs
+
+            //                          176/50 ~ 4 problems per hour
+
+            //                          */
 
             Console.ReadKey();
         }
