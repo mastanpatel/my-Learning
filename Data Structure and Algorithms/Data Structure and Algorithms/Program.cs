f3295d317b931422f503c39dbb261c7ba8bfdaca
mastanpatel <mastanpatel1993@gmail.com>
2019-05-05 05:58:09 +0530

    corrected file program.cs

--- Data Structure and Algorithms/Data Structure and Algorithms/Program.cs
+++ Data Structure and Algorithms/Data Structure and Algorithms/Program.cs
@@ -6,7 +6,6 @@ namespace Data_Structure_and_Algorithms
     {
         static void Main(string[] args)
         {
-
             /*   Data Strucutres:
              It is an Method of Storing and Organising a Data so that it can be used efficieantly.
 
@@ -32,64 +31,62 @@ namespace Data_Structure_and_Algorithms
 
 
             *** Very Important Lesson***//   //*** Interview wise and Competative Programing Wise***//
-            /* 
-            Take the Given Problem (Understand Problem thoroughly.
-            List Down Type of Data and Operations needed in this Problem and Size of the Data.
-            identify the most Heavily(Frequient) performed Operation (insertion, deletion, iteration etc...)
-            choose the Basic Data Strucutre mostly suited to implement above mevtioned Data strucuture and its Operations
-            with efficient cost (less costly)
-            choose to implement From primitive sata strucute like (Array, LinkedList, Stack, Queue, Tree, Graph, Hash etc....)
-            Optimise if any where Required.
+                                             /* 
+                                             Take the Given Problem (Understand Problem thoroughly.
+                                             List Down Type of Data and Operations needed in this Problem and Size of the Data.
+                                             identify the most Heavily(Frequient) performed Operation (insertion, deletion, iteration etc...)
+                                             choose the Basic Data Strucutre mostly suited to implement above mevtioned Data strucuture and its Operations
+                                             with efficient cost (less costly)
+                                             choose to implement From primitive sata strucute like (Array, LinkedList, Stack, Queue, Tree, Graph, Hash etc....)
+                                             Optimise if any where Required.
 
-           
-            Important Sylaabus for DS and Algorithms
-            
 
-      Data Structure and Algorithms
+                                             Important Sylaabus for DS and Algorithms
 
-    1. Arrays  -- 19
-       - Subarray with given sum -- (i have learn't to make code funtional and first solve for small input and them using brutforce or
-         whatever method hit u at that point
-         and then try to Optimise it (Time Comlexity and Space complexity) --> look at the Operations Performed on each element at worst case and try to reduce it.
-    
-       - Kadane’s Algorithm -- (solved it '\/' and learnt to minimise the steps (reduce the lines of code and remove unnessary things, use inbuild funtionality of c#,
-                                try to cover all corner cases.)
-    
-       - Missing number in array -- (solved using sumFormula)
-                                 -- did some mistakes
-    							    - Not reading Q's Properly (missing dat format like sorted or not and missing corner cases)
-    								- *** repeating same mistake of not rechecking code for errors and Optimisation.
-    								--- don't repeat above mistakes
-       - Sort an array of 0s, 1s and 2s -- (solved)
-    
-       - Equilibrium point -- (solved)   
-   
-       -    
-        
-    2. string -- 15
-    3. Linked List -- 17
-    4. stack and Queue -- 8
-    5. Tree and BST -- 16
-    6. Heap -- 6
-    7. Recursion -- 6
-    8. Hashing -- 15
-    9. Graph -- 15
-    10. Greedy -- 15
-    11. Dynamic Programming -- 15
-    12. Devide and Conquir -- 6
-    13. BackTracking -- 5
-    14. Bitmagic -- 14
 
-    -----------------------
-    Total :-- 176            avalable Time : 4 + 10 + 4 + 4 + 4 + 10 + 10 + 4 + 4 ~ 50 Hrs
+                                       Data Structure and Algorithms
 
-    176/50 ~ 4 problems per hour
+                                     1. Arrays  -- 19
+                                        - Subarray with given sum -- (i have learn't to make code funtional and first solve for small input and them using brutforce or
+                                          whatever method hit u at that point
+                                          and then try to Optimise it (Time Comlexity and Space complexity) --> look at the Operations Performed on each element at worst case and try to reduce it.
 
-    */
+                                        - Kadane’s Algorithm -- (solved it '\/' and learnt to minimise the steps (reduce the lines of code and remove unnessary things, use inbuild funtionality of c#,
+                                                                 try to cover all corner cases.)
+
+                                        - Missing number in array -- (solved using sumFormula)
+                                                                  -- did some mistakes
+                                                                     - Not reading Q's Properly (missing dat format like sorted or not and missing corner cases)
+                                                                     - *** repeating same mistake of not rechecking code for errors and Optimisation.
+                                                                     --- don't repeat above mistakes
+                                        - Sort an array of 0s, 1s and 2s -- (solved)
+
+                                        - Equilibrium point -- (solved)   
+
+                                        -    
+
+                                     2. string -- 15
+                                     3. Linked List -- 17
+                                     4. stack and Queue -- 8
+                                     5. Tree and BST -- 16
+                                     6. Heap -- 6
+                                     7. Recursion -- 6
+                                     8. Hashing -- 15
+                                     9. Graph -- 15
+                                     10. Greedy -- 15
+                                     11. Dynamic Programming -- 15
+                                     12. Devide and Conquir -- 6
+                                     13. BackTracking -- 5
+                                     14. Bitmagic -- 14
+
+                                     -----------------------
+                                     Total :-- 176            avalable Time : 4 + 10 + 4 + 4 + 4 + 10 + 10 + 4 + 4 ~ 50 Hrs
+
+                                     176/50 ~ 4 problems per hour
+
+                                     */
 
             Console.ReadKey();
-
-            
         }
     }
 }
